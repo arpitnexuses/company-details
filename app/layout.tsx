184cf2d715f9311d88986a24f8033507185b9470
arpitnexuses <arpit.m@nexuses.in>
2025-10-10 15:13:36 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Nexuses App',
+  description: 'Created with Nexuses',
+  generator: 'Nexuses',
 }
 
 export default function RootLayout({
